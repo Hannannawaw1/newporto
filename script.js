0d7abc0711282d2c5d986b29cf23584baeadaa34
Hannannawaw1 <hnawawi939@gmail.com>
2022-11-20 09:33:38 +0700

    updates

---
+++ script.js
@@ -0,0 +1,29 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector(".navbar-dark");
+window.onscroll = function () {
+  let top = window.scrollY;
+  if (top >= 600) {
+    header.classList.add("navbarDark");
+  } else {
+    header.classList.remove("navbarDark");
+  }
+};
+
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll(".nav-item");
+const menuToggle = document.getElementById("navbarSupportedContent");
+
+navLinks.forEach((l) => {
+  l.addEventListener("click", () => {
+    new bootstrap.Collapse(menuToggle).toggle();
+  });
+});
+
+const textRun = $(".job").typeIt({
+  strings: ["Junior Frontend Web", "Junior Network Engineer", "IT Lovers"],
+  speed: 170,
+  breakLines: false,
+  autoStart: false,
+  loop: true,
+});
+
